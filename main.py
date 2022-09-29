4459b631e509c07d155133e3d07591c7267616b8
seoyeun0106 <sarah.mun16@gmail.com>
2022-09-29 14:46:41 +0900

    second

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 name = input("이름을 입력하세요: ")
 num = input("학번을 입력하세요: ")
 major =input("학과를 입력하세요: ")
+<<<<<<< HEAD
 school =input("학교를 입력하세요: ")
 print("<출력>\n"+f"이름: {name}")
+print(f"학교: {school}")
+=======
+age =input("학년을 입력하세요: ")
 print(f"학번: {num}")
 print(f"학과: {major}")
-print(f"학교: {school}")
+print(f"학년: {age}")
+>>>>>>> ce4abaa (2022-09-28 실습과제#5 5번)
